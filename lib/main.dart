755eec3770225a72e701512d0652ac3c6ae350fc
Shah Newaz <shahnewazaziz2018@gmail.com>
2023-03-12 20:59:51 +0600

    Web Tell added

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,18 @@ import 'web_view.dart';
 
 void main() {
   runApp(const MyApp());
+
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
+
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shakib Sir',
-      debugShowCheckedModeBanner: false,
+
+      debugShowCheckedModeBanner: true,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
--- lib/web_view.dart
+++ lib/web_view.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+import 'dart:async';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class WebViewX extends StatefulWidget {
   const WebViewX({super.key});
@@ -8,8 +15,12 @@ class WebViewX extends StatefulWidget {
   State<WebViewX> createState() => _WebViewXState();
 }
 
+
+
+
 class _WebViewXState extends State<WebViewX> {
   double _progress = 0.0;
+
   late InAppWebViewController _webViewController;
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   @override
@@ -64,9 +75,50 @@ class _WebViewXState extends State<WebViewX> {
         children: [
           InAppWebView(
             initialUrlRequest: URLRequest(
-                url: Uri.parse("https://axismathematics.com/portal")),
+                url: Uri.parse("https://axismathematics.com/portal"),
+             // navigationeDlegate: _interceptNavigation,
+            ),
+
+            initialOptions: InAppWebViewGroupOptions(
+              crossPlatform: InAppWebViewOptions(
+
+                  useOnDownloadStart: true,
+                  useShouldOverrideUrlLoading: true
+              ),
+            ),
+
+            shouldOverrideUrlLoading: (controller, request) async {
+              var url = request.url;
+              var uri = Uri.parse(url);
+
+              if (!["http", "https", "file",
+                "chrome", "data", "javascript",
+                "about"].contains(uri.scheme)) {
+                if (await canLaunch(url)) {
+                  // Launch the App
+                  await launch(
+                    url,
+                  );
+                  // and cancel the request
+                  return ShouldOverrideUrlLoadingAction.CANCEL;
+                }
+              }
+
+              return ShouldOverrideUrlLoadingAction.ALLOW;
+            },
+
+            onDownloadStart: (controller, url,) async {
+              // print("onDownloadStart $url");
+              final String _url_files = "$url";
+              void _launchURL_files() async =>
+                  await canLaunch(_url_files) ? await launch(_url_files) : throw 'Could not launch $_url_files';
+              _launchURL_files();
+            },
+
+
             onWebViewCreated: (InAppWebViewController controller) {
               _webViewController = controller;
+
             },
             onProgressChanged:
                 (InAppWebViewController controller, int progress) {
@@ -86,3 +138,17 @@ class _WebViewXState extends State<WebViewX> {
     );
   }
 }
+
+
+
+NavigationDecision _interceptNavigation(NavigationRequest request) {
+  if (request.url.contains("mailto:")) {
+    launchUrlString('mailto:specify email address here');
+    return NavigationDecision.prevent;
+  } else if (request.url.contains("tel:")) {
+    launchUrlString('tel:specify telephone number here');
+    return NavigationDecision.prevent;
+  }
+  return NavigationDecision.navigate;
+}
+
